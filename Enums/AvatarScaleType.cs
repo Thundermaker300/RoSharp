f78fd5ac0db44444a2df45d5c5640f5dfbffce81
Thunder <thundermaker300@gmail.com>
2024-12-11 15:43:19 -0500

    Update AvatarScaleType.cs
    Docs

--- Enums/AvatarScaleType.cs
+++ Enums/AvatarScaleType.cs
@@ -6,13 +6,39 @@ using System.Threading.Tasks;
 
 namespace RoSharp.Enums
 {
+    /// <summary>
+    /// Represents different types of avatar scaling settings.
+    /// </summary>
     public enum AvatarScaleType
     {
+        /// <summary>
+        /// The height of the avatar.
+        /// </summary>
         Height,
+
+        /// <summary>
+        /// The width of the avatar.
+        /// </summary>
         Width,
+
+        /// <summary>
+        /// The head size of the avatar.
+        /// </summary>
         Head,
+
+        /// <summary>
+        /// The depth of the avatar.
+        /// </summary>
         Depth,
+
+        /// <summary>
+        /// The proportions of the avatar.
+        /// </summary>
         Proportion,
+
+        /// <summary>
+        /// The body type of the avatar.
+        /// </summary>
         BodyType,
     }
 }
