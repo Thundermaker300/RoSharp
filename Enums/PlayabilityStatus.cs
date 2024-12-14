120ee893617ec51c09643233399c436175e22a70
Thunder <thundermaker300@gmail.com>
2024-12-13 20:24:59 -0500

    Add more PlayabilityStatus from Roblox API

--- Enums/PlayabilityStatus.cs
+++ Enums/PlayabilityStatus.cs
@@ -26,7 +26,7 @@
         GuestProhibited,
 
         /// <summary>
-        /// Game is playable but must be purchased first.
+        /// Experience is playable but must first be purchased with Robux.
         /// </summary>
         /// <seealso cref="API.Assets.Experiences.Experience.Cost"/>
         PurchaseRequired,
@@ -52,10 +52,20 @@
         InsufficientPermissionFriendsOnly,
 
         /// <summary>
+        /// Experience is Group-Only and the authenticated user is not in the group.
+        /// </summary>
+        InsufficientPermissionGroupOnly,
+
+        /// <summary>
         /// Experience is 17+ and authenticated user is not age verified.
         /// </summary>
         /// <remarks>Seemingly unused and replaced by <see cref="ContextualPlayabilityAgeGatedByDescriptor"/>.</remarks>
         ContextualPlayabilityUnverifiedSeventeenPlusUser,
+
+        /// <summary>
+        /// Experience is unrated and authenticated user is not 13 or older.
+        /// </summary>
+        ContextualPlayabilityUnrated,
 
         /// <summary>
         /// Experience is 17+ and authenticated user is not age verified.
@@ -66,6 +76,46 @@
         /// Experience is marked with a higher "maturity level" than is allowed in the authenticated user's content maturity settings, OR the experience's content maturity is "N/A" and the user is under 13.
         /// </summary>
         ContextualPlayabilityAgeRecommendationParentalControls,
+
+        /// <summary>
+        /// Experience is unavailable in the authenticated user's region.
+        /// </summary>
+        ContextualPlayabilityRegionalAvailability,
+
+        /// <summary>
+        /// Authenticated user cannot play this experience to comply with local regulations.
+        /// </summary>
+        ContextualPlayabilityRegionalCompliance,
+
+        /// <summary>
+        /// Experience is playable but must first be purchased with local fiat currency.
+        /// </summary>
+        FiatPurchaseRequired,
+
+        /// <summary>
+        /// Experience is temporarily unavailable.
+        /// </summary>
+        TemporarilyUnavailable,
+
+        /// <summary>
+        /// Experience is not configured correctly.
+        /// </summary>
+        IncorrectConfiguration,
+
+        /// <summary>
+        /// An account moderation restriction prevents the authenticated user from playing this experience.
+        /// </summary>
+        AccountRestricted,
+
+        /// <summary>
+        /// Authenticated user cannot play this experience to comply with local regulations.
+        /// </summary>
+        ComplianceBlocked,
+
+        /// <summary>
+        /// Root place in an experience is not published.
+        /// </summary>
+        PlaceHasNoPublishedVersion,
 
     }
 }
