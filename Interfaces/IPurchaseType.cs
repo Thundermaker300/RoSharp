beaf0676cd9bf07d4d07fbf4e5423e102f49f84e
Thunder <thundermaker300@gmail.com>
2024-12-16 23:01:45 -0500

    Better IPurchaseType docs

--- Interfaces/IPurchaseType.cs
+++ Interfaces/IPurchaseType.cs
@@ -1,4 +1,5 @@
 ﻿using RoSharp.Enums;
+using RoSharp.Structures.PurchaseTypes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,26 @@ namespace RoSharp.Interfaces
 {
     /// <summary>
     /// Represents a type of purchase on the Roblox website.
+    /// <para>
+    /// Casts to the following structs:<br/>
+    /// * <see cref="FiatPurchase"/> - For purchases with local currency, defined in USD.<br/>
+    /// * <see cref="FreePurchase"/> - For purchases that are free!<br/>
+    /// * <see cref="NotForSalePurchase"/> - Placeholder for purchases that are not currently for sale.<br/>
+    /// * <see cref="RobuxPurchase"/> - For purchases with Robux.<br/>
+    /// * <see cref="UnknownPurchase"/> - Unknown purchase type, typically shouldn't be possible.
+    /// </para>
     /// </summary>
+    /// <example>
+    /// <example>
+    /// This code shows how to check an item's Robux price.
+        /// <code>
+        /// int robuxPrice = asset.PurchaseInfo is RobuxPurchase robux ? robux.Price : 0;
+        /// </code>
+    /// This code shows how to see if an item is on sale.
+        /// <code>
+        /// bool onSale = asset.PurchaseInfo is not NotForSalePurchase;
+        /// </code>
+    /// </example>
     public interface IPurchaseType
     {
         /// <summary>
