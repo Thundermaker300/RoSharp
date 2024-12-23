7f9cbef79d21cb0dfd494dd9cc545df9aa90b12a
Thunder <thundermaker300@gmail.com>
2024-12-23 16:39:40 -0500

    Update SaleLocationType.cs

--- Enums/SaleLocationType.cs
+++ Enums/SaleLocationType.cs
@@ -8,7 +8,27 @@
         /// <summary>
         /// Unknown.
         /// </summary>
-        NotApplicable = -1,
+        NotApplicable = 0,
+
+        /// <summary>
+        /// Can only be purchased in the shop.
+        /// </summary>
+        ShopOnly = 1,
+
+        /// <summary>
+        /// Can only be purchased within owner's experiences.
+        /// </summary>
+        MyExperiencesOnly = 2,
+
+        /// <summary>
+        /// Can be purchased in the shop and owner's experiences.
+        /// </summary>
+        ShopAndMyExperiences = 3,
+        
+        /// <summary>
+        /// Can only be purchased within certain experiences.
+        /// </summary>
+        ExperiencesById = 4,
 
         /// <summary>
         /// Can be purchased anywhere.
@@ -19,5 +39,15 @@
         /// Can only be purchased in certain <see cref="API.Assets.Experiences.Experience"/>s.
         /// </summary>
         AllowedGames = 6,
+
+        /// <summary>
+        /// Unknown.
+        /// </summary>
+        ExperiencesDevApiOnly = 6,
+
+        /// <summary>
+        /// Can only be purchased in the shop and within certain experiences.
+        /// </summary>
+        ShopAndExperiencesById = 7,
     }
 }
