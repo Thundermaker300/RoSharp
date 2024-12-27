b2b82a685c84a598e43044bc9a8b5533b23b9dcf
Thunder <thundermaker300@gmail.com>
2024-12-27 16:42:13 -0500

    Docs + more props

--- Structures/Color.cs
+++ Structures/Color.cs
@@ -1,19 +1,47 @@
 ﻿namespace RoSharp.Structures
 {
+    /// <summary>
+    /// Struct representing a color.
+    /// </summary>
     public readonly struct Color
     {
+
         private readonly string hexCode;
         private readonly (byte, byte, byte) rgbCombo;
 
+        /// <summary>
+        /// Gets the hex code representing the color.
+        /// </summary>
         public string HexCode => hexCode;
+
+        /// <summary>
+        /// Gets the red component of the color.
+        /// </summary>
         public byte R => rgbCombo.Item1;
+
+        /// <summary>
+        /// Gets the green component of the color.
+        /// </summary>
         public byte G => rgbCombo.Item2;
+
+        /// <summary>
+        /// Gets the blue component of the color.
+        /// </summary>
         public byte B => rgbCombo.Item3;
+
+        /// <summary>
+        /// Gets a string that can be used to display the R, G, and B values.
+        /// </summary>
+        public string RGBString => $"{R},{G},{B}";
+
+        /// <summary>
+        /// Gets an integer representing the specific <see cref="HexCode"/>.
+        /// </summary>
+        public int HexInt => Convert.ToInt32(HexCode, 16);
 
         private void Validate()
         {
-            int rgbInt = Convert.ToInt32(HexCode, 16);
-            if (rgbInt < 0 || rgbInt > 16777215)
+            if (HexInt < 0 || HexInt > 16777215)
             {
                 throw new InvalidOperationException("Invalid color.");
             }
@@ -21,10 +49,9 @@
 
         private (byte, byte, byte) GetRGB()
         {
-            int rgbInt = Convert.ToInt32(HexCode, 16);
-            byte red = (byte)((rgbInt >> 16) & 255);
-            byte green = (byte)((rgbInt >> 8) & 255);
-            byte blue = (byte)(rgbInt & 255);
+            byte red = (byte)((HexInt >> 16) & 255);
+            byte green = (byte)((HexInt >> 8) & 255);
+            byte blue = (byte)(HexInt & 255);
             return (red, green, blue);
         }
 
@@ -52,5 +79,18 @@
 
             Validate();
         }
+
+
+        // Static colors
+
+        /// <summary>
+        /// Represents a color with HexCode <c>000000</c>.
+        /// </summary>
+        public static Color Black { get; } = new();
+
+        /// <summary>
+        /// Represents a color with HexCode <c>FF0000</c>.
+        /// </summary>
+        public static Color Red { get; } = new("FF0000");
     }
 }
